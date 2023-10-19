9df5deed1374e5474c68e712351c7a538e47fe34
Sander <73292023+sander-van-damme@users.noreply.github.com>
2023-10-19 18:06:47 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# upgrade system
+sudo apt update
+sudo apt upgrade -y
+
+# install redis
+sudo apt install -y redis
+
+# install webdis
+cd
+sudo apt install -y libevent-dev git
+git clone git://github.com/nicolasff/webdis.git
+cd webdis
+make
+make install
+
+# enable webdis
+cd
+git clone https://github.com/wie-volgt-jou-op-het-internet/raspberry-pi.git
+cd raspberry-pi
+sudo cp webdis.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable webdis.service
+sudo systemctl start webdis.service
